3212065fdb68a94435f263939e3f3a57f3f486ee
Tyler Degand <tylerdegand@gmail.com>
2020-08-25 12:00:39 -0500

    setting up initial files

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
 
+//import routes
+const routes = require('./routes.js');
+
 //serves all static files
-app.use('/static', express.static('public'))
+app.use(express.static('public'))
 
 //sets view engine to render pug templates
 app.set('view engine', 'pug')
 
+//uses the routes file to route the user to the proper page
+app.use(routes)
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, "0.0.0.0");
