dea98ff4aae4c4bf95ee38c02765eff1cf482d64
Tyler Degand <tylerdegand@gmail.com>
2020-08-25 21:47:38 -0500

    should be good to go. (ready for development push)

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 //import routes
-const routes = require('./router.js');
+const routes = require("./router.js");
 
 //serves all static files
-app.use(express.static('public'))
-app.use(express.static('stylesheet'))
+app.use(express.static("public"));
+app.use(express.static("stylesheet"));
 
 //sets view engine to render pug templates
-app.set('view engine', 'pug')
+app.set("view engine", "pug");
 
 //uses the routes file to route the user to the proper page
-app.use(routes)
+app.use(routes);
 
 //handles errors
 app.use(function (err, req, res, next) {
-    console.error(error.stack)
-})
+	console.error(error.stack);
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, "0.0.0.0");
