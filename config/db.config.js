f095d92fa8da66848137cec0264fef8edeb077c3
Tyler Degand <tylerdegand@gmail.com>
2020-08-28 09:44:33 -0500

    removed unneeded file

--- config/db.config.js
+++
@@ -1,8 +0,0 @@
-mysql://b1bab50d205abf:93c1d433@us-cdbr-east-02.cleardb.com/heroku_9336adf963b1786?reconnect=true
-
-module.exports = {
-    HOST: "us-cdbr-east-02.cleardb.com",
-    USER: "b1bab50d205abf",
-    PASSWORD: "93c1d433",
-    DB: "heroku_9336adf963b1786"
-  };
